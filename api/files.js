6c6fb031156e7700beef46133533bdf837a094af
kimgrishuk-hash <kimgrishuk@icloud.com>
2026-08-26 15:46:05 +0300

    Allow product image storage actions

--- api/files.js
+++ api/files.js
@@ -1,3 +1,3 @@
 const {proxyEdge}=require('./_proxy');
-const ACTIONS=new Set(['prepare_upload','download_url','delete']);
+const ACTIONS=new Set(['prepare_upload','product_image','download_url','delete']);
 module.exports=(req,res)=>proxyEdge(req,res,'clinic-files',ACTIONS,128*1024);
