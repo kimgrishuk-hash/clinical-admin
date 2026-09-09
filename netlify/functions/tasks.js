318454ae24fc0a9b237835888149505d2fc4bba9
kimgrishuk-hash <kimgrishuk@icloud.com>
2026-09-09 21:50:26 +0300

    Support filtered task reads and task details

--- netlify/functions/tasks.js
+++ netlify/functions/tasks.js
@@ -1,3 +1,3 @@
 const {proxyEdge}=require('./_common');
-const ACTIONS=new Set(['list','create','comment','close','reopen','delete']);
+const ACTIONS=new Set(['list','get','create','update','comment','comment_update','comment_delete','close','reopen','delete']);
 exports.handler=(event)=>proxyEdge(event,'clinic-tasks',ACTIONS,512*1024);
