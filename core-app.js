61205fbaf866a6ddd014d3e148b6b7fe00b322d5
kimgrishuk-hash <kimgrishuk@icloud.com>
2026-09-10 11:56:53 +0300

    Load modern clinic UI directly on every page load

--- core-app.js
+++ core-app.js
@@ -60,3 +60,4 @@ document.write('<script src="core-app-legacy.js?v=4"><'+'/script>');
 document.write('<script src="universal-edit.js?v=1"><'+'/script>');
 document.write('<script src="aml-pricebook.js?v=1"><'+'/script>');
 document.write('<script src="image-storage.js?v=1"><'+'/script>');
+document.write('<script src="home-ui.js?v=7" data-dynamic="home-ui"><'+'/script>');
